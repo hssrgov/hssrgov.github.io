0dc416fc2703617968d34227c8101ce1c38a5503
hssrgov <88079797+hssrgov@users.noreply.github.com>
2022-08-06 23:14:51 +0800

    有必要专门学习计算机知识
    rt

--- bdohlh/show.js
+++ bdohlh/show.js
@@ -13,12 +13,13 @@ const dg=["类人型哈儿","高智商哈儿","中智商哈儿","低智商哈儿
 //           0             1            2       3           4            5         6           7         8           9
 const dgclr=["#00FF00","#AA00FF","#FFFF00","#00FFFF","#CC0000","#D06A0D","#6B6895","#2A5750","#FFFFFF","#88AAEE"];//每个ХИРГ的对应颜色
 const clrs=["#FFAAAA","#FFFFAA","#AAFFAA","#AAFFFF","#FFAAFF","#AAFFFF"];//链接颜色，共6个；数据库存储文件URL
-const ne_title="不支持 - 哈儿实验室危险哈儿数据库 БДОХЛХ",ne_body="<h1 style=\" color: #FFAAAA;\">浏览器版本太旧，请使用更高版本的浏览器</h1>",XMLFILEURL_ORI="/bdohlh/haerlib.xml";//База Данных Опасных Хаеры Лаборатории Хаера=БДОХЛХ
-var hname,born,des,dgs,info,haerona,dgmhx,age,die,xmlfileurl;//hname：名称（临时存储用的变量），born：出生年份，des：概要，dgs：ХИРГ（临时存储用的变量），info：信息（描述），haerona：此哈儿的负责人，dgmhx：哈儿性质（0=哈儿，1=哈儿团，2=哈儿簇，临时存储用的变量）,age：年龄（临时存储用的变量），die：被销毁或死亡年份（临时存储用的变量，用以标注年龄），xmlfileurl：XML文件路径
+const ne_title="不支持 - 哈儿实验室危险哈儿数据库 БДОХЛХ",ne_body="<h1 style=\" color: #FFAAAA;\">浏览器版本太旧，请使用更高版本的浏览器</h1>",XMLFILEURL_ORI="bdohlh/haerlib.xml";//База Данных Опасных Хаеры Лаборатории Хаера=БДОХЛХ
+var hname,born,des,dgs,info,haerona,dgmhx,age,die,xmlfileurl,localurl=toString(window.top.location.href);//hname：名称（临时存储用的变量），born：出生年份，des：概要，dgs：ХИРГ（临时存储用的变量），info：信息（描述），haerona：此哈儿的负责人，dgmhx：哈儿性质（0=哈儿，1=哈儿团，2=哈儿簇，临时存储用的变量）,age：年龄（临时存储用的变量），die：被销毁或死亡年份（临时存储用的变量，用以标注年龄），xmlfileurl：XML文件路径，localurl：当前主域名
 var mhn,minhn,thisyear,actualminhn,rg,qj;//mhn为最大编号，minhn为最小编号，直接影响到索引的表格行数和“当前可用编号”的数值；thisyear为当前年份（公历），用于计算哈儿的年龄；actualminhn为实际最小哈儿编号；rg为循环替换如[br]之类的简略符号的循环次数，32次足够多；可用哈儿区间
 var xmlDoc,date=new Date();
 thisyear=date.getFullYear();//加载当前年份
-xmlfileurl=window.top.location.href+XMLFILEURL_ORI;//加载XML文件地址
+if(localurl.charAt(localurl.length-1)!="/")localurl+="/";
+xmlfileurl=localurl+XMLFILEURL_ORI;//加载XML文件地址
 console.log("[Info]: [thisyear: "+thisyear+" xmlfileurl:"+xmlfileurl+"]");
 function about()//输出版权信息
 {
